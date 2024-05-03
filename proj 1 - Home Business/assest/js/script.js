1fe1d1d4bd835c098892b00e83910272913d86eb
Manikandaraj T N <93505267+manikandaraj-T-N@users.noreply.github.com>
2024-05-03 14:23:17 +0530

    Add files via upload

---
+++ proj 1 - Home Business/assest/js/script.js
@@ -0,0 +1,23 @@
+$('#owl-carousel').owlCarousel({
+    loop: true,
+    margin: 30,
+    dots: true,
+    nav: true,
+   
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:false
+        }
+    }
+})
